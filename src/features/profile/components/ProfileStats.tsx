30dfced12a292a31fe35b19bbac924811468a798
Mohamed Abdelnaser <mohamed@example.com>
2026-03-06 15:30:16 +0200

    Solve confilcets

--- src/features/profile/components/ProfileStats.tsx
+++ src/features/profile/components/ProfileStats.tsx
@@ -13,28 +13,22 @@ const ProfileStats: React.FC<Props> = ({ profileId }) => {
 
   if (!profile) return null;
 
-<<<<<<< HEAD
   // pick only the last three stats you want to display
   if (!profile || typeof profile !== "object" || !("countriesCount" in profile))
     return null;
 
-=======
->>>>>>> ed4e3453388f43d9d01ecf37e5066adbd79a5464
   const statsToShow = [
     { label: "Countries", value: profile.countriesCount },
     { label: "Followers", value: profile.followersCount },
     { label: "Following", value: profile.followingCount },
   ];
-<<<<<<< HEAD
   console.log(
     "ProfileStats - profile:",
     profile.followersCount,
     profile.followingCount,
     profile.countriesCount,
   );
-=======
-  console.log(profileId)
->>>>>>> ed4e3453388f43d9d01ecf37e5066adbd79a5464
+
   return (
     <motion.div variants={container} initial="hidden" animate="show" className="mt-6 flex justify-between bg-gray-50 rounded-xl p-4 shadow-sm">
       {statsToShow.map((stat) => (
