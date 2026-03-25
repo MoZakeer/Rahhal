75e2844c54447b0992dd9184d8413023cebc7bb6
Reem Mohamed <ream2004523farouk@gmail.com>
2026-03-25 16:33:42 +0200

    fix errors

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,14 +1,13 @@
 import { GripVertical } from "lucide-react";
-import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
+import * as Resizable from "react-resizable-panels";
 import * as React from "react";
-
 import { cn } from "@/lib/utils";
 
 const ResizablePanelGroup = ({
   className,
   ...props
-}: React.ComponentProps<typeof PanelGroup>) => (
-  <PanelGroup
+}: React.ComponentProps<typeof Resizable.PanelGroup>) => (
+  <Resizable.PanelGroup
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
       className
@@ -17,18 +16,18 @@ const ResizablePanelGroup = ({
   />
 );
 
-const ResizablePanel = Panel;
+const ResizablePanel = Resizable.Panel;
 
 const ResizableHandle = ({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof PanelResizeHandle> & {
+}: React.ComponentProps<typeof Resizable.PanelResizeHandle> & {
   withHandle?: boolean;
 }) => (
-  <PanelResizeHandle
+  <Resizable.PanelResizeHandle
     className={cn(
-      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 [&[data-panel-group-direction=vertical]>div]:rotate-90",
+      "relative flex w-px items-center justify-center bg-border",
       className
     )}
     {...props}
@@ -38,7 +37,7 @@ const ResizableHandle = ({
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
-  </PanelResizeHandle>
+  </Resizable.PanelResizeHandle>
 );
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle };
