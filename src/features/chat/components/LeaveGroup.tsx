27872e6310e10b26d12ef64d0578feb174164e3a
Mohamed Abdelnaser <mohamed@example.com>
2026-04-11 10:13:47 +0200

    Add leave group feature

--- src/features/chat/components/LeaveGroup.tsx
+++ src/features/chat/components/LeaveGroup.tsx
@@ -1,11 +1,27 @@
+import { useParams } from "react-router";
+import { useLeavGroup } from "../hooks/useLeaveGroup";
+import { useUser } from "@/context/UserContext";
+
 function LeaveGroup() {
+  const { conversationId } = useParams<{ conversationId: string }>();
+  const {
+    user: { userId: profileId },
+  } = useUser();
+  const { isPending, leaveGroup } = useLeavGroup({
+    conversationId: conversationId || "",
+    profileId,
+  });
   return (
     <div className="pt-4 border-t border-gray-200 space-y-4">
       <p className="text-xs font-semibold tracking-wide text-gray-400 uppercase">
         Danger Zone
       </p>
 
-      <button className="text-red-600 text-sm font-medium hover:underline cursor-pointer hover:text-red-700 outline-none">
+      <button
+        className="text-red-600 text-sm font-medium hover:underline cursor-pointer hover:text-red-700 outline-none disabled:text-red-700 disabled:cursor-not-allowed"
+        disabled={isPending}
+        onClick={() => leaveGroup()}
+      >
         Leave group
       </button>
     </div>
---
+++ src/features/chat/hooks/useLeaveGroup.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { leaveGroup as leaveGroupApi } from "../services/leaveGroup";
+import { useNavigate } from "react-router";
+
+export function useLeavGroup({
+  conversationId,
+  profileId,
+}: {
+  conversationId: string;
+  profileId: string;
+}) {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { isPending, mutate: leaveGroup } = useMutation({
+    mutationFn: () => leaveGroupApi({ conversationId, profileId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["chat"],
+      });
+      navigate("/chat");
+    },
+  });
+  return { isPending, leaveGroup };
+}
