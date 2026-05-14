349cbf9bef22e2231b93e790a4addcb5765160f1
Mohamed Abdelnaser <mohamed@example.com>
2026-05-14 18:03:52 +0300

    fix chat header

--- src/features/chat/components/ChatHeader.tsx
+++ src/features/chat/components/ChatHeader.tsx
@@ -49,12 +49,12 @@ function ChatHeader({
       date.toLocaleDateString([], {
         day: "2-digit",
         month: "2-digit",
-        year: "numeric",
+        year: "2-digit",
       }) + ` at ${time}`
     );
   };
   return (
-    <div className="bg-gray-0 flex items-center  gap-4 border-b  border-solid border-gray-200 shadow-sm px-4 py-2 w-full sm:px-6 sm:py-3">
+    <div className="bg-gray-0 flex items-center gap-2 md:gap-4 border-b  border-solid border-gray-200 shadow-sm px-4 py-2 w-full sm:px-6 sm:py-3">
       <button
         className="rounded-full p-1 text-gray-800 hover:bg-gray-100 flex items-center justify-center md:hidden"
         onClick={() => navigate("/chat")}
