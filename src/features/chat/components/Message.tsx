94b507e9d900d15d50a9474a2ed2940718d45a6b
Mohamed Abdelnaser <mohamed@example.com>
2026-05-22 15:05:22 +0300

    update viberation

--- src/features/chat/components/Message.tsx
+++ src/features/chat/components/Message.tsx
@@ -93,7 +93,7 @@ function Message({
       onReply(message);
 
       if (window.navigator?.vibrate) {
-        window.navigator.vibrate(50);
+        window.navigator.vibrate(20);
       }
     }
   };
@@ -176,7 +176,7 @@ function Message({
   const longPressEvents = useLongPress(
     () => {
       if (window.navigator?.vibrate) {
-        window.navigator.vibrate(30);
+        window.navigator.vibrate(20);
       }
       setOpenedReactionId(message.messageId);
       setOpenedMenuId(null);
