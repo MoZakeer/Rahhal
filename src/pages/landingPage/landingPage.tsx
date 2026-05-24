0378b111276fac0da8a40b427386337fedd8f90b
Mahmoud Abdelrazek <mahmoudzanitty@gmail.com>
2026-05-24 11:14:39 +0300

    feat(ui): massive landing page redesign with interactive mockups and framer-motion

--- src/pages/landingPage/landingPage.tsx
+++ src/pages/landingPage/landingPage.tsx
@@ -4,8 +4,10 @@ import CTAsection from "./components/CTAsection";
 // import TravelStoriesSection from "./components/travelStoriesSection";
 import Navbar from '../../shared/components/navbar';
 import Footer from '../../shared/components/footer';
+import { useFavicon } from "@/hooks/useFavicon";
 
 export default function LandingPage() {
+  useFavicon("light-logo.png")
   return (
     <div className="min-h-screen w-full bg-[var(--color-gray-0)] text-[var(--color-gray-900)]">
 
