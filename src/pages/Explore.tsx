848524b5013ffb5fb9f28726841e039790bd3703
Mahmoud Abdelrazek <mahmoudzanitty@gmail.com>
2026-05-24 18:57:38 +0300

    solve deploy

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -7,7 +7,7 @@ import {
   PenLine,
 } from "lucide-react";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import TripCard from "@/components/TripCard";
 import heroImage from "@/assets/hero-travel.jpg";
