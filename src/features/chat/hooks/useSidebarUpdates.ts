838e42632ee99307f8ac73ed68c743a79377c7c3
zakeer <zakeer@gmail.com>
2026-05-30 19:04:12 +0300

    solve reaction bug

--- src/features/chat/hooks/useSidebarUpdates.ts
+++ src/features/chat/hooks/useSidebarUpdates.ts
@@ -42,7 +42,7 @@ export const useSidebarUpdates = function () {
     const handleUpdateSidebar = function (data: UpdateSidebarData) {
       queryClient.setQueryData<{ data: ChatType[] }>(
         ["all-chats"],
-        (oldData) => {
+        (oldData:any) => {
           if (!oldData?.data) return oldData;
 
           const chats = [...oldData.data];
@@ -60,6 +60,7 @@ export const useSidebarUpdates = function () {
               unreadMessagesCount: isChatOpenNow ? 0 : data.unreadCount,
               lastMessageType: data.messageType,
               lastMessageSender: data.lastMessageSender,
+              reactionInfo:null,
             };
 
             chats.splice(index, 1);
